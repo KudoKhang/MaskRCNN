9d09ad58759ecde09ce4f6e87b3008de8e8be1d7
KudoKhang <thanhchem.k39a1@gmail.com>
2022-09-20 14:55:18 +0700

    alive_progess

--- networks/libs.py
+++ networks/libs.py
@@ -12,6 +12,7 @@ from numpy.random import uniform
 import random
 import argparse
 import time
+from alive_progress import alive_bar
 import wandb
 os.environ["WANDB_API_KEY"] = 'e7ed558aefc5cddf29d04c3037a712507b253521'
 
